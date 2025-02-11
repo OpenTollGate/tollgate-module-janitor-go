6bd3f6c279f2e841c747d0a4f7f1cf1de41648bb
Your Name <you@example.com>
2025-02-11 08:51:55 +0100

    Added version number

--- src/main.go
+++ src/main.go
@@ -1,6 +1,30 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "runtime/debug"
+)
+
+var (
+    Version   string
+    CommitHash string
+    BuildTime string
+)
+
+func getVersionInfo() string {
+    if info, ok := debug.ReadBuildInfo(); ok {
+        for _, setting := range info.Settings {
+            switch setting.Key {
+            case "vcs.revision":
+                CommitHash = setting.Value[:7]
+            case "vcs.time":
+                BuildTime = setting.Value
+            }
+        }
+    }
+    return fmt.Sprintf("Version: %s\nCommit: %s\nBuild Time: %s", 
+        Version, CommitHash, BuildTime)
+}
 
 func main() {
 	fmt.Println("hello world 2")
